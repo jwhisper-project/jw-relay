d59d38cb7ba7840cb54c1c34e4e33562a1946728
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-03 15:50:21 +0200

    Create config manager

---
+++ src/main/java/io/github/artshp/jwhisper/relay/ConfigManager.java
@@ -0,0 +1,50 @@
+package io.github.artshp.jwhisper.relay;
+
+import io.github.artshp.jwhisper.relay.exception.ConfigFileException;
+import lombok.extern.slf4j.Slf4j;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.SerializationFeature;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+public class ConfigManager {
+
+    private static final String CONFIG_FILE = "config.json";
+    private static final Path CONFIG_FILE_PATH = Path.of(CONFIG_FILE);
+
+    private static final ObjectMapper mapper = JsonMapper.builder()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .build();
+
+    public void saveConfig(ServerConfig config) throws ConfigFileException {
+        log.debug("Trying to save config to {}", CONFIG_FILE_PATH);
+        try {
+            mapper.writeValue(CONFIG_FILE_PATH, config);
+            log.info("Successfully saved config to {}", CONFIG_FILE_PATH);
+        } catch (JacksonException e) {
+            log.error("Failed to save config to {}", CONFIG_FILE_PATH, e);
+            throw new ConfigFileException("Failed to save config file", e);
+        }
+    }
+
+    public ServerConfig loadConfig() throws ConfigFileException {
+        log.debug("Trying to load config from {}", CONFIG_FILE_PATH);
+        try {
+            ServerConfig config = mapper.readValue(CONFIG_FILE_PATH, ServerConfig.class);
+            log.info("Successfully loaded config from {}", CONFIG_FILE_PATH);
+
+            return config;
+        } catch (JacksonException e) {
+            log.error("Failed to load config from {}", CONFIG_FILE_PATH, e);
+            throw new ConfigFileException("Failed to load config file", e);
+        }
+    }
+
+    public boolean isConfigPresent() {
+        return Files.exists(CONFIG_FILE_PATH);
+    }
+}
