19f2fa1fc12b8524f884094b1e5108a4d70f88e8
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-03 13:41:19 +0200

    Add wrong password exception

---
+++ src/main/java/io/github/artshp/jwhisper/relay/exception/WrongPasswordException.java
@@ -0,0 +1,10 @@
+package io.github.artshp.jwhisper.relay.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * Exception for wrong provided password.
+ */
+@StandardException
+public class WrongPasswordException extends RuntimeException {
+}
