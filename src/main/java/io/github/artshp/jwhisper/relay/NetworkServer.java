803ea909a72eaaffc12906ed31369b124dac4ef5
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-08 14:12:19 +0200

    Add basic requests processing

--- src/main/java/io/github/artshp/jwhisper/relay/NetworkServer.java
+++ src/main/java/io/github/artshp/jwhisper/relay/NetworkServer.java
@@ -1,13 +1,21 @@
 package io.github.artshp.jwhisper.relay;
 
 import io.github.artshp.jwhisper.common.crypto.SecurityUtils;
+import io.github.artshp.jwhisper.common.crypto.SigningUtils;
+import io.github.artshp.jwhisper.common.exception.NetworkServiceException;
 import io.github.artshp.jwhisper.common.protocol.MessageTransport;
+import io.github.artshp.jwhisper.common.protocol.RegisterRequest;
+import io.github.artshp.jwhisper.common.protocol.StatusResponse;
+import io.github.artshp.jwhisper.common.protocol.WhisperMessage;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.*;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -52,7 +60,7 @@ public class NetworkServer implements AutoCloseable {
         log.info("Starting Relay Server on {}:{}", serverSocket.getInetAddress(), serverSocket.getLocalPort());
         while (true) {
             try {
-                executorService.submit(new Servant((SSLSocket) serverSocket.accept()));
+                executorService.submit(new Servant((SSLSocket) serverSocket.accept(), transport));
             } catch (IOException e) {
                 log.error("Failed to accept or close a connection", e);
             }
@@ -66,23 +74,71 @@ public class NetworkServer implements AutoCloseable {
         }
     }
 
-    private class Servant implements Runnable {
+    private static class Servant implements Runnable {
 
         private final SSLSocket socket;
+        private final MessageTransport transport;
 
-        public Servant(SSLSocket socket) {
+        public Servant(SSLSocket socket, MessageTransport transport) {
             this.socket = socket;
+            this.transport = transport;
         }
 
         @Override
         public void run() {
             try (SSLSocket socket = this.socket) {
                 log.info("Accepted connection from {}", socket.getInetAddress());
+
+                WhisperMessage response = receive();
+                try {
+                    if (response instanceof RegisterRequest request) {
+                        processRegisterRequest(request);
+                    }
+
+                    switch (response) {
+                        case RegisterRequest request -> processRegisterRequest(request);
+                        default -> throw new NetworkServiceException("Unexpected response: " + response);
+                    }
+                } catch (NetworkServiceException e) {
+                    log.error("Failed to process request", e);
+                }
             } catch (IOException e) {
                 log.error("Error during communication with relay", e);
             }
 
             log.info("Closing connection from {}", socket.getInetAddress());
+        }
+
+        private void processRegisterRequest(RegisterRequest request) throws NetworkServiceException, IOException {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(request.publicKey());
+            PublicKey publicKey;
+            try {
+                publicKey = SecurityUtils.KEY_FACTORY.generatePublic(keySpec);
+            } catch (InvalidKeySpecException e) {
+                throw new NetworkServiceException("Failed to generate public key.", e);
+            }
+
+            boolean valid = SigningUtils.verify(
+                    publicKey,
+                    request.username().getBytes(),
+                    request.usernameSignature()
+            );
+
+            // TODO: replace with real logic
+            if (!valid) {
+                log.error("Failed to verify public key");
+            } else {
+                StatusResponse statusResponse = new StatusResponse(true, "Registered successfully");
+                send(statusResponse);
+            }
+        }
+
+        public void send(WhisperMessage message) throws IOException {
+            transport.sendMessage(socket.getOutputStream(), message);
+        }
+
+        public WhisperMessage receive() throws IOException {
+            return transport.receiveMessage(socket.getInputStream(), WhisperMessage.class);
         }
     }
 }
