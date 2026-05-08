f0bdea58a31b0da513d7f045038ded0b058425f0
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-08 10:05:35 +0200

    Refactor reading user input

--- src/main/java/io/github/artshp/jwhisper/relay/ServerApp.java
+++ src/main/java/io/github/artshp/jwhisper/relay/ServerApp.java
@@ -1,12 +1,11 @@
 package io.github.artshp.jwhisper.relay;
 
+import io.github.artshp.jwhisper.common.exception.InputRetryException;
+import io.github.artshp.jwhisper.common.io.UserInputUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.KeyManagerFactory;
-import java.io.Console;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
 
 /**
  * Server app class.
@@ -14,40 +13,29 @@ import java.util.Optional;
 @Slf4j
 class ServerApp {
 
-    private final Console console;
     private final ConfigManager configManager = new ConfigManager();
 
     /**
      * Constructs a new server application.
      */
     public ServerApp() {
-        console = System.console();
-        if (console == null) {
-            log.error("Console is not available.");
-            throw new IllegalStateException("Console is not available.");
-        }
     }
 
     /**
      * Start server application.
      */
-    public void start() {
+    public void start() throws InputRetryException {
         log.info("Starting Relay Server");
         System.out.println("----- JWhisper Relay -----");
 
-        Optional<char[]> password = readPassword(false);
-        if (password.isEmpty()) {
-            log.error("Failed to get password.");
-            return;
-        }
-
-        KeyManagerFactory keyManagerFactory = IdentityManager.getKeyManagerFactory(password.get());
+        char[] password = UserInputUtils.readPassword();
+        KeyManagerFactory keyManagerFactory = IdentityManager.getKeyManagerFactory(password);
 
         ServerConfig config;
         if (!configManager.isConfigPresent()) {
             log.debug("No config present. Creating it...");
 
-            int port = readPort();
+            int port = UserInputUtils.readPort();
 
             config = new ServerConfig(port);
             configManager.saveConfig(config);
@@ -61,63 +49,5 @@ class ServerApp {
         } catch (IOException e) {
             log.error("Failed to close network server.", e);
         }
-    }
-
-    private int readPort() {
-        String portString = console.readLine("Enter port: ");
-        return portString.isEmpty() ? 8080 : Integer.parseInt(portString);
-    }
-
-    private static boolean isPasswordValid(char[] password) {
-        return password.length >= 4;
-    }
-
-    private Optional<char[]> readPassword(boolean repeat) {
-        char[] password;
-
-        boolean isValid;
-        int i = 0;
-        do {
-            password = console.readPassword("Enter password: ");
-            if (!isPasswordValid(password)) {
-                isValid = false;
-                log.warn("Password length should be at least 4. Try again.");
-            } else {
-                isValid = true;
-            }
-
-            i++;
-        } while (!isValid && i < 3);
-
-        if (!isValid) {
-            log.error("Provided password is invalid.");
-            return Optional.empty();
-        }
-
-        if (!repeat) {
-            return Optional.of(password);
-        }
-
-        char[] passwordRetry;
-
-        i = 0;
-        do {
-            passwordRetry = console.readPassword("Re-enter password: ");
-            if (!Arrays.equals(password, passwordRetry)) {
-                isValid = false;
-                log.warn("Passwords are not equal. Try again.");
-            } else {
-                isValid = true;
-            }
-
-            i++;
-        } while (!isValid && i < 3);
-
-        if (!isValid) {
-            log.warn("You failed to repeat the password.");
-            return Optional.empty();
-        }
-
-        return Optional.of(password);
     }
 }
