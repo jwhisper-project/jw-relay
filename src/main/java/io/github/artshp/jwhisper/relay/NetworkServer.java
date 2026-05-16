1268fc9be002fb89049b247e7b3021353df98965
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-16 16:20:01 +0200

    feat: add users communication (#4)

--- src/main/java/io/github/artshp/jwhisper/relay/NetworkServer.java
+++ src/main/java/io/github/artshp/jwhisper/relay/NetworkServer.java
@@ -9,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.*;
 import java.io.IOException;
+import java.net.Socket;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,6 +25,7 @@ public class NetworkServer implements AutoCloseable {
     private final UserRegistry userRegistry = new UserRegistry();
     private final MessageTransport transport = new MessageTransport();
     private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+    private final Map<Socket, Servant> users = new ConcurrentHashMap<>();
 
     private final SSLServerSocketFactory serverSocketFactory;
     private final SSLServerSocket serverSocket;
@@ -76,6 +79,10 @@ public class NetworkServer implements AutoCloseable {
     private class Servant implements Runnable {
 
         private final SSLSocket socket;
+        private final Object sendLock = new Object();
+        private final Object receiveLock = new Object();
+
+        private String username = null;
 
         public Servant(SSLSocket socket) {
             this.socket = socket;
@@ -92,6 +99,8 @@ public class NetworkServer implements AutoCloseable {
                     try {
                         switch (response) {
                             case RegisterRequest request -> processRegisterRequest(request);
+                            case UserPublicKeyRequest request -> processUserPublicKeyRequest(request);
+                            case EncryptedMessage message -> routeMessage(message);
                             case UnregisterRequest _ -> {
                                 processUnregisterRequest();
                                 isRunning = false;
@@ -111,19 +120,25 @@ public class NetworkServer implements AutoCloseable {
         }
 
         private void processRegisterRequest(RegisterRequest request) throws NetworkServiceException, IOException {
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(request.publicKey());
-            PublicKey publicKey;
+            PublicKey publicSigningKey;
             try {
-                publicKey = SecurityUtils.KEY_FACTORY.generatePublic(keySpec);
+                publicSigningKey = SecurityUtils.newSigningPublicKey(request.publicSigningKey());
             } catch (InvalidKeySpecException e) {
                 throw new NetworkServiceException("Failed to generate public key.", e);
             }
 
-            String username = request.username();
+            PublicKey publicEncryptionKey;
+            try {
+                publicEncryptionKey = SecurityUtils.newEncryptionPublicKey(request.publicEncryptionKey());
+            } catch (InvalidKeySpecException e) {
+                throw new NetworkServiceException("Failed to generate public key.", e);
+            }
+
+            username = request.username();
             boolean valid = SigningUtils.verify(
-                    publicKey,
+                    publicSigningKey,
                     username.getBytes(),
-                    request.usernameSignature()
+                    request.ownershipSignature()
             );
 
             if (!valid) {
@@ -134,14 +149,54 @@ public class NetworkServer implements AutoCloseable {
                     send(new StatusResponse(false, "Username already taken"));
                 } else {
                     LogContext.setUsername(username);
-                    userRegistry.register(socket, username, publicKey);
+                    userRegistry.register(socket, username, publicSigningKey, publicEncryptionKey);
+                    users.put(socket, this);
                     send(new StatusResponse(true, "Registered successfully"));
                 }
             }
         }
 
+        private void processUserPublicKeyRequest(UserPublicKeyRequest request) throws IOException {
+            String username = request.targetUsername();
+            log.info("Received user public key request of user {}", username);
+
+            PublicKey publicSigningKey = userRegistry.getUserPublicSigningKey(username);
+            if (publicSigningKey != null) {
+                log.info("Successfully found public keys of user {}", username);
+                PublicKey publicEncryptionKey = userRegistry.getUserPublicEncryptionKey(username);
+                send(new UserPublicKeyResponse(
+                        username, publicSigningKey.getEncoded(), publicEncryptionKey.getEncoded(), true
+                ));
+            } else {
+                log.error("Failed to find public keys of user {}", username);
+                send(new UserPublicKeyResponse(username, null, null, false));
+            }
+        }
+
+        private void routeMessage(EncryptedMessage encryptedMessage) throws IOException {
+            String recipient = encryptedMessage.recipient();
+            log.info("Received encrypted message addressed to {}", recipient);
+
+            Socket recipientSocket = userRegistry.getSocket(recipient);
+            if (recipientSocket != null) {
+                log.info("Sending message to {}", recipient);
+                Servant recipientServant = users.get(recipientSocket);
+                recipientServant.send(new UserPublicKeyResponse(
+                        username,
+                        userRegistry.getUserPublicSigningKey(username).getEncoded(),
+                        userRegistry.getUserPublicEncryptionKey(username).getEncoded(),
+                        true
+                ));
+                recipientServant.send(encryptedMessage);
+                log.info("Sent message to {}", recipient);
+            } else {
+                log.error("Failed to send encrypted message to {}", recipient);
+            }
+        }
+
         private void processUnregisterRequest() throws IOException {
             if (userRegistry.unregister(socket)) {
+                users.remove(socket);
                 send(new StatusResponse(true, "Unregistered successfully"));
             } else {
                 send(new StatusResponse(false, "Failed to unregister user"));
@@ -149,11 +204,15 @@ public class NetworkServer implements AutoCloseable {
         }
 
         public void send(WhisperMessage message) throws IOException {
-            transport.sendMessage(socket.getOutputStream(), message);
+            synchronized (sendLock) {
+                transport.sendMessage(socket.getOutputStream(), message);
+            }
         }
 
         public WhisperMessage receive() throws IOException {
-            return transport.receiveMessage(socket.getInputStream(), WhisperMessage.class);
+            synchronized (receiveLock) {
+                return transport.receiveMessage(socket.getInputStream(), WhisperMessage.class);
+            }
         }
     }
 }
--- src/main/java/io/github/artshp/jwhisper/relay/storage/UserRegistry.java
+++ src/main/java/io/github/artshp/jwhisper/relay/storage/UserRegistry.java
@@ -11,30 +11,56 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserRegistry {
 
     /**
-     * Map of usernames to their public keys
+     * Map of usernames to their public signing keys
      */
-    private final Map<String, PublicKey> users = new ConcurrentHashMap<>();
+    private final Map<String, PublicKey> userPublicSigningKeys = new ConcurrentHashMap<>();
+
+    /**
+     * Map of usernames to their public encryption keys
+     */
+    private final Map<String, PublicKey> userPublicEncryptionKeys = new ConcurrentHashMap<>();
 
     /**
      * Map of sockets to their usernames
      */
     private final Map<Socket, String> sockets = new ConcurrentHashMap<>();
 
+    /**
+     * Map of usernames to their sockets
+     */
+    private final Map<String, Socket> sockets2 = new ConcurrentHashMap<>();
+
     public boolean isUsernameTaken(String username) {
-        return users.containsKey(username);
+        return userPublicSigningKeys.containsKey(username);
     }
 
-    public void register(Socket socket, String username, PublicKey publicKey) {
-        users.put(username, publicKey);
+    public void register(Socket socket, String username, PublicKey publicSigningKey, PublicKey publicEncryptionKey) {
+        userPublicSigningKeys.put(username, publicSigningKey);
+        userPublicEncryptionKeys.put(username, publicEncryptionKey);
         sockets.put(socket, username);
+        sockets2.put(username, socket);
         log.info("User {} registered successfully", username);
+    }
+
+    public PublicKey getUserPublicSigningKey(String username) {
+        return userPublicSigningKeys.get(username);
+    }
+
+    public PublicKey getUserPublicEncryptionKey(String username) {
+        return userPublicEncryptionKeys.get(username);
+    }
+
+    public Socket getSocket(String username) {
+        return sockets2.get(username);
     }
 
     public boolean unregister(Socket socket) {
         String username = sockets.get(socket);
         if (username != null) {
-            users.remove(username);
+            userPublicSigningKeys.remove(username);
+            userPublicEncryptionKeys.remove(username);
             sockets.remove(socket);
+            sockets2.remove(username);
             log.info("User {} unregistered successfully", username);
             return true;
         } else {
