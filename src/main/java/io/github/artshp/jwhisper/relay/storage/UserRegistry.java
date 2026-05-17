b2b4f337d42b2f4683d61c0c9837bbb3365291c8
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-17 12:47:47 +0200

    Add UserRegistry javadoc

--- src/main/java/io/github/artshp/jwhisper/relay/storage/UserRegistry.java
+++ src/main/java/io/github/artshp/jwhisper/relay/storage/UserRegistry.java
@@ -7,60 +7,101 @@ import java.security.PublicKey;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Users registry. Manages currently registered (logged in) users.
+ */
 @Slf4j
 public class UserRegistry {
 
     /**
-     * Map of usernames to their public signing keys
+     * Map of usernames to their public signing keys.
      */
     private final Map<String, PublicKey> userPublicSigningKeys = new ConcurrentHashMap<>();
 
     /**
-     * Map of usernames to their public encryption keys
+     * Map of usernames to their public encryption keys.
      */
     private final Map<String, PublicKey> userPublicEncryptionKeys = new ConcurrentHashMap<>();
 
     /**
-     * Map of sockets to their usernames
+     * Map of sockets to their usernames.
      */
     private final Map<Socket, String> sockets = new ConcurrentHashMap<>();
 
     /**
-     * Map of usernames to their sockets
+     * Map of usernames to their sockets.
      */
-    private final Map<String, Socket> sockets2 = new ConcurrentHashMap<>();
+    private final Map<String, Socket> socketsReverse = new ConcurrentHashMap<>();
 
+    /**
+     * Create a new user registry.
+     */
+    public UserRegistry() {
+    }
+
+    /**
+     * Is given username taken, i.e. is there any registered user with the same username?
+     * @param username username to check
+     * @return {@code true} if username is taken, otherwise {@code false}
+     */
     public boolean isUsernameTaken(String username) {
         return userPublicSigningKeys.containsKey(username);
     }
 
+    /**
+     * Register user.
+     * @param socket client socket.
+     * @param username user's username
+     * @param publicSigningKey user's public signing key
+     * @param publicEncryptionKey user's public encryption key
+     */
     public void register(Socket socket, String username, PublicKey publicSigningKey, PublicKey publicEncryptionKey) {
         userPublicSigningKeys.put(username, publicSigningKey);
         userPublicEncryptionKeys.put(username, publicEncryptionKey);
         sockets.put(socket, username);
-        sockets2.put(username, socket);
+        socketsReverse.put(username, socket);
         LOGGER.info("User {} registered successfully", username);
     }
 
+    /**
+     * Get user's public signing key.
+     * @param username target user username
+     * @return public signing key of target user if registered, otherwise {@code null}
+     */
     public PublicKey getUserPublicSigningKey(String username) {
         return userPublicSigningKeys.get(username);
     }
 
+    /**
+     * Get user's public encryption key.
+     * @param username target user username
+     * @return public encryption key of target user if registered, otherwise {@code null}
+     */
     public PublicKey getUserPublicEncryptionKey(String username) {
         return userPublicEncryptionKeys.get(username);
     }
 
+    /**
+     * Get client's socket.
+     * @param username target user username
+     * @return socket to target user if registered, otherwise {@code null}
+     */
     public Socket getSocket(String username) {
-        return sockets2.get(username);
+        return socketsReverse.get(username);
     }
 
+    /**
+     * Unregister user.
+     * @param socket client socket.
+     * @return {@code true} if unregistered user successfully, otherwise {@code false}
+     */
     public boolean unregister(Socket socket) {
         String username = sockets.get(socket);
         if (username != null) {
             userPublicSigningKeys.remove(username);
             userPublicEncryptionKeys.remove(username);
             sockets.remove(socket);
-            sockets2.remove(username);
+            socketsReverse.remove(username);
             LOGGER.info("User {} unregistered successfully", username);
             return true;
         } else {
