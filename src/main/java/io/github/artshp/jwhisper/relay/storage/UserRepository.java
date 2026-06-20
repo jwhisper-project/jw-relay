46474f1d5c9cc611b320a72edbc2e1e6a7f69907
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-06-20 11:03:40 +0200

    Update user repository

--- src/main/java/io/github/artshp/jwhisper/relay/storage/UserRepository.java
+++ src/main/java/io/github/artshp/jwhisper/relay/storage/UserRepository.java
@@ -3,6 +3,12 @@ package io.github.artshp.jwhisper.relay.storage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
