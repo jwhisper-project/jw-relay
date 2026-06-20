efbaecee46f957856623886af9f63f1349bb678a
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-06-20 12:00:05 +0200

    Add Spring context bridge

---
+++ src/main/java/io/github/artshp/jwhisper/relay/util/SpringContextBridge.java
@@ -0,0 +1,20 @@
+package io.github.artshp.jwhisper.relay.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextBridge implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        context = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> beanClass) {
+        return context.getBean(beanClass);
+    }
+}
